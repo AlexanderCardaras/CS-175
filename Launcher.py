c9e46a0da3e2f9722649703f197708e025cc50de
Alexander Cardaras <acardara@uci.edu>
2020-02-14 01:00:24 -0800

    Updated to demonstrate logistic regressor

--- Launcher.py
+++ Launcher.py
@@ -1,19 +1,44 @@
-from data_parser import tokenizer
+from data_parser import haiku_parser as hp
+from ml import logistic_regressor as lr
 
 raw_data_path = "res/raw_data.json"         # US code html
 parsed_data_path = "res/parsed_data.txt"    # US code parsed
 constitution_path = "res/constitution.txt"  # Constitution parsed
+haiku_path = "res/haiku.csv"                # Constitution parsed
 
-# laws = tokenizer.strip_json(raw_data_path)
-# tokenizer.list_to_file(laws, parsed_data_path)
+""" N-gram demo """
+# data = tokenizer.file_to_list(parsed_data_path)
+# n_grams = tokenizer.create_n_grams(data, max_n_gram=1)
+# print(n_grams)
 
-max_n = 7
+""" Logistic regression demo """
+poems, syllables = hp.get_poems(haiku_path, sample_size=1)
+poems, syllables = hp.filter_poems(poems, syllables)
+model = lr.train(poems)
 
-data = tokenizer.file_to_list(parsed_data_path)
-n_grams = tokenizer.create_n_grams(data, max_n_gram=max_n)
+print("\n\nSamples:")
+print("poem:", poems[0])
+print("input:", poems[0][0])
+print("prediction:", lr.classify(poems[0][0], model))
+print("input:", poems[0][1])
+print("prediction:", lr.classify(poems[0][1], model))
+print("input:", poems[0][2])
+print("prediction:", lr.classify(poems[0][2], model))
 
-data = tokenizer.file_to_list(constitution_path)
-n_grams = tokenizer.create_n_grams(data, n_grams=n_grams, max_n_gram=max_n)
+print("\n")
+print("poem:", poems[1])
+print("input:", poems[1][0])
+print("prediction:", lr.classify(poems[1][0], model))
+print("input:", poems[1][1])
+print("prediction:", lr.classify(poems[1][1], model))
+print("input:", poems[1][2])
+print("prediction:", lr.classify(poems[1][2], model))
 
-for n in n_grams.keys():
-    print("n={} {}".format(n, len(n_grams[n])))
+print("\n")
+print("poem:", poems[2])
+print("input:", poems[2][0])
+print("prediction:", lr.classify(poems[2][0], model))
+print("input:", poems[2][1])
+print("prediction:", lr.classify(poems[2][1], model))
+print("input:", poems[2][2])
+print("prediction:", lr.classify(poems[2][2], model))
